9766a7a093686d4cb33692691fa5acbb80dbeeaf
rafaguzmang <55261233+rafaguzmang@users.noreply.github.com>
2025-01-06 17:19:17 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Calidad',
+    'version': '1.0',
+    'description': 'Modulo para llevar las actividades de calidad',
+    'author': 'Rafael Guzmán Granados',
+    'depends': ["base"],
+    'data': [
+        'views/dtm_calidad_rechazo_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False
+}
---
+++ models/dtm_calidad.py
@@ -0,0 +1,16 @@
+from odoo import fields,api,models
+from datetime import datetime
+
+
+class Calidad(models.Model):
+    _name = 'dtm.calidad.rechazo'
+    _description = 'Registro de calidad'
+
+    job_no = fields.Char(string='JOB NO')
+    po_number = fields.Char(string='P. O. NO.')
+    part_no = fields.Char(string='PART NO')
+    no_of_pieces_rejected = fields.Integer(string='NO. OF PIECES REJECTED')
+    reason = fields.Char(string='REASON')
+    inspector = fields.Char(string='INSPECTOR')
+    date = fields.Date(string='DATE',default=datetime.now())
+
